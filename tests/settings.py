640e529f5d7b7ea8830e2d80e1d762fa3ba49007
Simon Barnett <sbarnett@acresofspace.co.uk>
2021-02-23 14:55:43 +0100

    Added newly created app dir and runtests.py

---
+++ tests/settings.py
@@ -0,0 +1,12 @@
+SECRET_KEY = 'fake-key'
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+    }
+}
+
+INSTALLED_APPS = [
+    "rest_email_manager",
+]
---
+++ tests/test_models.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+
+# from rest_email_manager import models
+
+
+class TestModels(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
