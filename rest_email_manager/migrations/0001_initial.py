8ce96a2b69f6de69bc2cb6f11f06ae0716566007
Simon Barnett <sbarnett@acresofspace.co.uk>
2021-02-24 15:11:06 +0100

    black fixes

--- rest_email_manager/migrations/0001_initial.py
+++ rest_email_manager/migrations/0001_initial.py
@@ -16,20 +16,56 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='EmailAddress',
+            name="EmailAddress",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('email', models.EmailField(max_length=254)),
-                ('verified', models.BooleanField(default=False)),
-                ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='emailaddresses', to=settings.AUTH_USER_MODEL)),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("email", models.EmailField(max_length=254)),
+                ("verified", models.BooleanField(default=False)),
+                (
+                    "user",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="emailaddresses",
+                        to=settings.AUTH_USER_MODEL,
+                    ),
+                ),
             ],
         ),
         migrations.CreateModel(
-            name='EmailAddressVerification',
+            name="EmailAddressVerification",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('key', models.CharField(default=rest_email_manager.models.generate_key, max_length=255)),
-                ('emailaddress', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='verifications', to='rest_email_manager.emailaddress')),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                (
+                    "key",
+                    models.CharField(
+                        default=rest_email_manager.models.generate_key,
+                        max_length=255,
+                    ),
+                ),
+                (
+                    "emailaddress",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="verifications",
+                        to="rest_email_manager.emailaddress",
+                    ),
+                ),
             ],
         ),
     ]
